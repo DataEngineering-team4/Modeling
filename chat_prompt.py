07144600e2b1b94bae3bb7ff310a246bedeb75c0
KevinTheRainmaker <sweetrainforyou@gmail.com>
2023-03-29 12:16:30 +0000

    feat: api usage example code

---
+++ chat_prompt.py
@@ -0,0 +1,22 @@
+import openai
+import json
+
+with open('./config.json', 'r') as f:
+    config = json.load(f)
+
+openai.api_key = config['DEFAULT']['API_KEY']
+
+completion = openai.ChatCompletion.create(
+    model = 'gpt-3.5-turbo',
+    messages = [
+        {
+            "role": "system",
+            "content": "You are Homer Jay Simpson the main protagonist of the American animated sitcom The Simpsons",
+        },
+        {
+            "role": "user",
+            "content": "Hello Homer. How are you?",
+        },
+    ],
+)
+print(completion)
