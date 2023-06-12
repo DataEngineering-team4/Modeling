644531dd03903d9d164311d2e7048bb38a2fe881
KevinTheRainmaker <sweetrainforyou@gmail.com>
2023-06-12 08:56:45 +0000

    feat: device setting

--- serving/app/api/router/predict.py
+++ serving/app/api/router/predict.py
@@ -6,6 +6,10 @@ import numpy as np
 from fastapi import APIRouter, Request
 from pathlib import Path
 
+physical_devices = tf.config.list_physical_devices('GPU')
+if len(physical_devices) > 0:
+    tf.config.experimental.set_memory_growth(physical_devices[0], True)
+
 router = APIRouter(
     prefix="/ai/dev/team4/predict",
     tags=["predict"],
